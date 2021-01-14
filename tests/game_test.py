df9680ee67dfd68c8bcdf51bcaf3eac3b6214d71
Scott Anderson <ix.scott.anderson@gmail.com>
2021-01-14 13:53:56 +0000

    add more game tests

--- tests/game_test.py
+++ tests/game_test.py
@@ -6,6 +6,7 @@ class TestGame(unittest.TestCase):
 
     def SetUp(self):
         game = Game("primary")
+        game2 = Game("PRIMARY")
 
     def test_solution_length(self):
         self.assertEqual(7, game.length())
@@ -73,3 +74,72 @@ class TestGame(unittest.TestCase):
         self.assertEqual(True, result)
         self.assertEqual(True, game.won())
         self.assertEqual("PRIMARY", game.display)
+
+
+    def test_solution_length_upper_solution(self):
+        self.assertEqual(7, game2.length())
+
+    def test_display_upper_solution(self):
+        self.assertEqual("_______", game2.display)
+
+    def test_wrong_letter_upper_solution(self):
+        result = game2.try_letter('b')
+        self.assertEqual(False, result)
+        self.assertEqual("_______", game2.display)
+
+    def test_correct__letter_upper_solution(self):
+        result = game2.try_letter('i')
+        self.assertEqual(True, result)
+        self.assertEqual("__I____", game2.display)
+
+    def test_upper_letter_upper_solution(self):
+        result = game2.try_letter('I')
+        self.assertEqual(True, result)
+        self.assertEqual("__I____", game2.display)
+
+    def test_multi_letter_upper_solution(self):
+        result = game2.try_letter('r')
+        self.assertEqual(True, result)
+        self.assertEqual("_R___R_", game2.display)
+
+    def test_first_letter_upper_solution(self):
+        result = game2.try_letter('p')
+        self.assertEqual(True, result)
+        self.assertEqual("P______", game2.display)
+
+    def test_last_letter_upper_solution(self):
+        result = game2.try_letter('y')
+        self.assertEqual(True, result)
+        self.assertEqual("______Y", game2.display)
+
+    def test_not_won_upper_solution(self):
+        self.assertEqual(False, game2.won())
+
+    def test_try_letter_all_upper_solution(self):
+
+        for letter in "primary":
+            result = game2.try_letter(letter)
+            self.assertEqual(True, result)
+
+        self.assertEqual(True, game2.won())
+        self.assertEqual("PRIMARY", game2.display)
+
+    def test_try_word_wrong_upper_solution(self):
+        result = game2.try_word("secondary")
+        self.assertEqual(False, result)
+        self.assertEqual(False, game2.won())
+        self.assertEqual("_______", game2.display)
+
+    
+    def test_try_word_correct_upper_solution(self):
+        result = game2.try_word("primary")
+        self.assertEqual(True, result)
+        self.assertEqual(True, game2.won())
+        self.assertEqual("PRIMARY", game2.display)
+
+    def test_try_word_correct_upper_upper_solution(self):
+        result = game2.try_word("PRIMARY")
+        self.assertEqual(True, result)
+        self.assertEqual(True, game2.won())
+        self.assertEqual("PRIMARY", game2.display)
+
